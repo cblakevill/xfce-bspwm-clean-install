a77e661be88c14007d1731e95814ab57a23769d9
blake <cblakevill@gmail.com>
2017-11-25 20:50:17 +0800

    bug fix

--- search.sh
+++ search.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 query=$(cat ~/.config/sxhkd/site-suggestions.txt | rofi -dmenu)
-if [ $query != "" ]; then
-	if [[ $query =~ .*\..* ]]; then
-		firefox -new-tab $query
+if [ "$query" != "" ]; then
+	if [[ "$query" =~ .*\..* ]]; then
+		firefox -new-tab "$query"
 	else
 		firefox -private-window "https://www.google.com.au/search?q=$query"
 	fi
